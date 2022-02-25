99640df881b0d2c03c9f775008dce3d4f972a643
TanvirHridoy <anxioushridoy2@gmail.com>
2022-02-25 15:13:59 +0600

    Add project files.

---
+++ Endpoints/ExampleEndpoints.cs
@@ -0,0 +1,22 @@
+﻿using FastEndpoints;
+
+namespace FastMinimalApi.Endpoints
+{
+    public class ExampleEndpoints : EndpointWithoutRequest
+    {
+        public override void Configure()
+        {
+            Verbs(Http.GET);
+            Routes("api/example");
+            AllowAnonymous();
+
+
+                
+        }
+
+        public override async  Task HandleAsync(CancellationToken ct)
+        {
+            await SendAsync(new {name="Ani",Age=420},cancellation: ct);
+        }
+    }
+}
